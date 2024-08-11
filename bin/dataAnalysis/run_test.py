383d2999977c1b758bbe069b219e5a2400d80dc9
Goamhobala <youyasushi@youyasushis-MacBook-Air.local>
2024-08-11 14:29:04 +0200

    fixed bugs in .gitignore

--- bin/dataAnalysis/run_test.py
+++ bin/dataAnalysis/run_test.py
@@ -1,17 +1,35 @@
 import subprocess
 import sys
+import os
 """
 A python script to run multiple tests
 """
 
 def run_test(start, end, interval, value):
-    number_runs = (end - start) / 2
-    for i in range(number_runs):
-        dimension = start + interval * i
+    with open("./analyses/resultsSerial.csv", mode="w"):
+        # to initialise the csv
+        pass
+    with open("./analyses/resultsParallel.csv", mode="w"):
+        pass
+    number_runs = (int(end) - int(start)) // int(interval)
+    for i in range(number_runs + 1):
+        dimension = int(start) + int(interval) * i
         # To generate the csv file
-        subprocess.run(["make", "csv", f"CSV_INF={dimension}x{dimension}_{value}.csv {dimension} {dimension} {value}"])
-        # To run the trial
-        subprocess.run(["make", "run" ,"ARGS='input\{dimension}x{dimension}_{value}.csv output\{dimension}x{dimension}_{value}.png'"])
+        csv_info = f"{dimension}x{dimension}_{value}.csv {dimension} {dimension} {value}"
+        input_path = f"./input/{dimension}x{dimension}_{value}.csv"
+        print(input_path)
+        # For the parallel version
+        subprocess.run(["make", "csv", f"CSV_INFO=./input/{csv_info}"])
+            # To run the trial
+        subprocess.run(["make", "run_parallel" ,f'ARGS={input_path} ./output/parallel/{dimension}x{dimension}_{value}.png'])
+        
+        # For the serial version
+        subprocess.run(["make", "csv", f"CSV_INFO=./input/{csv_info}"])
+            # To run the trial
+        subprocess.run(["make", "run_serial" ,f'ARGS={input_path} ./output/serial/{dimension}x{dimension}_{value}.png'])
+        
+        # clean up the csv files generated for the trial
+        os.remove(input_path)
         
 
 if __name__ == "__main__":
